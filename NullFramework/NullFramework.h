f7f03ee4dbb0f8f7dcf0bf2703700c697f4aec98
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-27 20:57:24 +0800

    Initial commit

---
+++ NullFramework/NullFramework.h
@@ -0,0 +1,19 @@
+//
+//  NullFramework.h
+//  NullFramework
+//
+//  Created by Yunarta Kartawahyudi on 26/6/18.
+//  Copyright © 2018 mobilesolutionworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NullFramework.
+FOUNDATION_EXPORT double NullFrameworkVersionNumber;
+
+//! Project version string for NullFramework.
+FOUNDATION_EXPORT const unsigned char NullFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NullFramework/PublicHeader.h>
+
+
